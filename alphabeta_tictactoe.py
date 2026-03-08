1a404ba6995ed8d69d360187cd3dc3241f1f9d1f
Jagadesh <jsurneni@gitam.in>
2026-03-08 10:10:35 +0530

    Add Alpha-Beta Tic Tac Toe implementation
    Implement a Tic Tac Toe game using the Alpha-Beta pruning algorithm for the computer's moves.

---
+++ alphabeta_tictactoe.py
@@ -0,0 +1,98 @@
+import math
+
+board = [' ' for _ in range(9)]
+
+def print_board():
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print("-+-+-")
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print("-+-+-")
+    print(board[6] + "|" + board[7] + "|" + board[8])
+
+def check_winner(player):
+    win_conditions = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+    for condition in win_conditions:
+        if board[condition[0]] == board[condition[1]] == board[condition[2]] == player:
+            return True
+    return False
+
+def check_draw():
+    return ' ' not in board
+
+def alphabeta(is_maximizing, alpha, beta):
+    if check_winner('O'):
+        return 1
+    if check_winner('X'):
+        return -1
+    if check_draw():
+        return 0
+
+    if is_maximizing:
+        best_score = -math.inf
+        for i in range(9):
+            if board[i] == ' ':
+                board[i] = 'O'
+                score = alphabeta(False, alpha, beta)
+                board[i] = ' '
+                best_score = max(score, best_score)
+                alpha = max(alpha, score)
+                if beta <= alpha:
+                    break
+        return best_score
+
+    else:
+        best_score = math.inf
+        for i in range(9):
+            if board[i] == ' ':
+                board[i] = 'X'
+                score = alphabeta(True, alpha, beta)
+                board[i] = ' '
+                best_score = min(score, best_score)
+                beta = min(beta, score)
+                if beta <= alpha:
+                    break
+        return best_score
+
+def best_move():
+    best_score = -math.inf
+    move = 0
+    for i in range(9):
+        if board[i] == ' ':
+            board[i] = 'O'
+            score = alphabeta(False, -math.inf, math.inf)
+            board[i] = ' '
+            if score > best_score:
+                best_score = score
+                move = i
+    board[move] = 'O'
+
+while True:
+    print_board()
+    pos = int(input("Enter position (1-9): ")) - 1
+
+    if board[pos] == ' ':
+        board[pos] = 'X'
+    else:
+        print("Invalid move")
+        continue
+
+    if check_winner('X'):
+        print_board()
+        print("You win!")
+        break
+
+    if check_draw():
+        print_board()
+        print("Draw!")
+        break
+
+    best_move()
+
+    if check_winner('O'):
+        print_board()
+        print("Computer wins!")
+        break
